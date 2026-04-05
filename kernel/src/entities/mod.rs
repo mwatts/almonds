4efe97dfc984cd94f895f101490d6bcff3a08927
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-04-06 00:30:40 +0100

    chore

--- kernel/src/entities/mod.rs
+++ kernel/src/entities/mod.rs
@@ -2,12 +2,10 @@
 mod sqlite;
 #[cfg(feature = "sqlite")]
 pub use sqlite::*;
-
 #[cfg(feature = "postgres")]
 mod postgres;
 #[cfg(feature = "postgres")]
 pub use postgres::*;
-
 #[cfg(feature = "mysql")]
 mod mysql;
 #[cfg(feature = "mysql")]
