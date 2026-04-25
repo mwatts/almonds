a36e237212ce8d2e4de0ee4e50e935ec35d2200e
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2026-04-25 11:03:37 +0100

    lint

--- server/src/routes/country.rs
+++ server/src/routes/country.rs
@@ -11,6 +11,9 @@ pub(super) fn country_routes(state: ServicesState) -> Router {
     Router::new()
         .route("/", get(fetch_all_countries))
         .route("/{identifier}", get(fetch_country_by_identifier))
-        .route("/currency/{currency_code}", get(fetch_countries_by_currency_code))
+        .route(
+            "/currency/{currency_code}",
+            get(fetch_countries_by_currency_code),
+        )
         .with_state(state)
 }
