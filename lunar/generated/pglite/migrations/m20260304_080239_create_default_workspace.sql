dc2280fbccb4f3afecb09a4ba45747bc3dbea9f0
opeolluwa <adefemiadeoye@yahoo.com>
2026-08-15 12:09:57 +0100

    Update m20260304_080239_create_default_workspace.sql

--- lunar/generated/pglite/migrations/m20260304_080239_create_default_workspace.sql
+++ lunar/generated/pglite/migrations/m20260304_080239_create_default_workspace.sql
@@ -4,18 +4,18 @@
 
 
         INSERT INTO workspaces (identifier, name, description, created_at, updated_at)
-        SELECT '85b49460-f44a-47f4-930a-8d0b45ec34be', 'default', 'Default workspace', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+        SELECT '65be8e3a-7bc3-4c02-89fd-88bb2b85d029', 'default', 'Default workspace', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
         WHERE NOT EXISTS (
             SELECT 1 FROM workspaces WHERE name = 'default'
         );
         ;
 
 
-        UPDATE todo SET workspace_identifier = '85b49460-f44a-47f4-930a-8d0b45ec34be' WHERE workspace_identifier IS NULL;
-        UPDATE notes SET workspace_identifier = '85b49460-f44a-47f4-930a-8d0b45ec34be' WHERE workspace_identifier IS NULL;
-        UPDATE bookmark SET workspace_identifier = '85b49460-f44a-47f4-930a-8d0b45ec34be' WHERE workspace_identifier IS NULL;
-        UPDATE recycle_bin SET workspace_identifier = '85b49460-f44a-47f4-930a-8d0b45ec34be' WHERE workspace_identifier IS NULL;
-        UPDATE reminder SET workspace_identifier = '85b49460-f44a-47f4-930a-8d0b45ec34be' WHERE workspace_identifier IS NULL;
-        UPDATE snippets SET workspace_identifier = '85b49460-f44a-47f4-930a-8d0b45ec34be' WHERE workspace_identifier IS NULL;
+        UPDATE todo SET workspace_identifier = '65be8e3a-7bc3-4c02-89fd-88bb2b85d029' WHERE workspace_identifier IS NULL;
+        UPDATE notes SET workspace_identifier = '65be8e3a-7bc3-4c02-89fd-88bb2b85d029' WHERE workspace_identifier IS NULL;
+        UPDATE bookmark SET workspace_identifier = '65be8e3a-7bc3-4c02-89fd-88bb2b85d029' WHERE workspace_identifier IS NULL;
+        UPDATE recycle_bin SET workspace_identifier = '65be8e3a-7bc3-4c02-89fd-88bb2b85d029' WHERE workspace_identifier IS NULL;
+        UPDATE reminder SET workspace_identifier = '65be8e3a-7bc3-4c02-89fd-88bb2b85d029' WHERE workspace_identifier IS NULL;
+        UPDATE snippets SET workspace_identifier = '65be8e3a-7bc3-4c02-89fd-88bb2b85d029' WHERE workspace_identifier IS NULL;
         ;
 
